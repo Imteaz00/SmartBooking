c2cf7053eea89380cefceaadb4753ee392adbc0e
Imteaz00 <imteaz969@gmail.com>
2026-04-09 19:28:27 +0600

    refactor(fetch): simplify date formatting in fetchDailyProfitByMonth function

--- frontend/actions/fetchDailyProfitByMonth.ts
+++ frontend/actions/fetchDailyProfitByMonth.ts
@@ -4,10 +4,7 @@ import { BACKEND_URL } from "@/server";
 
 export async function fetchDailyProfitByMonth(date: Date) {
   try {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const day = String(date.getDate()).padStart(2, "0");
-    const formattedDate = `${year}-${month}-${day}`;
+    const formattedDate = date.toISOString().split("T")[0];
 
     const response = await fetch(`${BACKEND_URL}/schedule/dailyProfitByMonth/${formattedDate}`);
     if (!response.ok) {
